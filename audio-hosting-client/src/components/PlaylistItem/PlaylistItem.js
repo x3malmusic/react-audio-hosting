196cc3c7b49b37b8ba4ffeae9f277fcdbebd8d3d
x3malmusic <x3malmusic@gmail.com>
2021-08-31 17:03:03 +0300

    fix playlist item

--- audio-hosting-client/src/components/PlaylistItem/PlaylistItem.js
+++ audio-hosting-client/src/components/PlaylistItem/PlaylistItem.js
@@ -12,17 +12,17 @@ export default function PlaylistItem({ original_filename, duration, _id, current
 
   return (
     <ListItem
-      className={clsx(classes.playlistItem, { [classes.chosenSong]: currentSong._id === _id })}
+      className={clsx(classes.playlistItem, { [classes.chosenSong]: currentSong === index  })}
       onMouseOver={() => setHover(true)}
       onMouseLeave={() => setHover(false)}
     >
       <IconButton
         color="primary"
-        className={clsx(classes.playBtn, { [classes.playBtnHover]: currentSong._id === _id || hover })}
+        className={clsx(classes.playBtn, { [classes.playBtnHover]: currentSong === index || hover })}
         disabled={!hover}
         onClick={() => setSong(index)}
       >
-        {currentSong._id === _id ? <VolumeUpIcon /> : <PlayArrowIcon />}
+        {currentSong === index  ? <VolumeUpIcon /> : <PlayArrowIcon />}
       </IconButton>
       <Typography className={classes.name} noWrap>{original_filename}</Typography>
       <Typography className={classes.duration}>{makeDuration(duration)}</Typography>
