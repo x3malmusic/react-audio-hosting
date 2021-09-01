68a359fac7c5ee12b20318cc29acde5910dcaf00
x3malmusic <x3malmusic@gmail.com>
2021-09-01 12:30:12 +0300

    add new route

--- audio-hosting-client/src/routes.js
+++ audio-hosting-client/src/routes.js
@@ -1,5 +1,6 @@
 import AudiotrackIcon from '@material-ui/icons/Audiotrack';
 import PublishIcon from '@material-ui/icons/Publish';
+import PlaylistAddIcon from '@material-ui/icons/PlaylistAdd';
 
 import Main from "./pages/Main"
 import UploadTrack from "./pages/UploadTrack";
@@ -17,6 +18,12 @@ const routes =  [
     component: UploadTrack,
     Icon: PublishIcon,
     name: "Upload track",
+  },
+  {
+    path: "/create-playlist",
+    component: null,
+    Icon: PlaylistAddIcon,
+    name: "Create playlist",
   },
 ]
 
