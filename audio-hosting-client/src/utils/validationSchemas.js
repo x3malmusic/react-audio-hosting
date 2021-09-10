672a0ec9db79ab33d58a9a7991ae61020a40948d
x3malmusic <x3malmusic@gmail.com>
2021-09-10 12:34:47 +0300

    add register validation schema

--- audio-hosting-client/src/utils/validationSchemas.js
+++ audio-hosting-client/src/utils/validationSchemas.js
@@ -7,3 +7,19 @@ export const loginSchema = Yup.object().shape({
   password: Yup.string()
     .required("Enter password")
 })
+
+export const registerSchema = Yup.object().shape({
+  email: Yup.string()
+    .email("Email is not valid")
+    .required("Email is required"),
+  password: Yup.string()
+    .required("Enter password")
+    .length(6, "Password should be at least 6 characters long"),
+  repeatPassword: Yup.string().when("password", {
+    is: val => (val && val.length > 0),
+    then: Yup.string().oneOf(
+      [Yup.ref("password")],
+      "Both password need to be the same"
+    )
+  })
+})
