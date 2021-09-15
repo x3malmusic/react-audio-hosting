ccff3a605e260d1e5094bca8fb91ea8ebb842281
x3malmusic <x3malmusic@gmail.com>
2021-09-15 15:09:57 +0300

    add drawer with all songs in main

--- audio-hosting-client/src/pages/Main/Main.js
+++ audio-hosting-client/src/pages/Main/Main.js
@@ -1,17 +1,20 @@
 import React, { useContext, useEffect, useRef, useState } from "react";
-import { Box } from "@material-ui/core";
+import { Box, Drawer } from "@material-ui/core";
+import AllSongsList from "../../components/AllSongsList";
 import Playlist from "../../components/Playlist";
 import AppButton from "../../components/AppButton/AppButton";
 import ChoosePlaylistModal from "../../components/Modal/ChoosePlaylist";
+import SearchInput from "../../components/SearchInput";
+import { PlayerRefContext } from "../../context";
 import { ADD_PLAYLIST } from "../../components/Placeholder";
 import useStyles from "./styles";
-import { Context } from "../../context";
 
 export default function Main({ playlists, changePlaylist }) {
   const classes = useStyles();
   const playerContainerRef = useRef()
-  const playerRef = useContext(Context)
+  const playerRef = useContext(PlayerRefContext)
   const [openModal, setOpenModal] = useState(false)
+  const [openDrawer, setOpenDrawer] = useState(false)
 
   useEffect(() => {
     playerContainerRef.current.appendChild(playerRef.current)
@@ -19,11 +22,28 @@ export default function Main({ playlists, changePlaylist }) {
 
   return (
     <>
-      <Box className={classes.main} >
+      <Box className={classes.controls}>
+        <SearchInput className={classes.marginRight} />
+        <AppButton className={classes.marginRight}  onClick={() => setOpenModal(true)}>Open</AppButton>
+        <AppButton onClick={() => setOpenDrawer(!openDrawer)}>Songs</AppButton>
+      </Box>
+
+      <Box className={classes.main}>
         <Box ref={playerContainerRef} className={classes.playerContainer} />
         <Playlist placeholder={ADD_PLAYLIST} />
+
+        <Drawer
+          open={openDrawer}
+          anchor="bottom"
+          variant="persistent"
+          className={classes.drawer}
+          classes={{ paper: classes.drawerPaper }}
+          transitionDuration={{ enter: 1000, exit: 1000 }}
+        >
+          <AllSongsList />
+        </Drawer>
       </Box>
-      <AppButton onClick={() => setOpenModal(true)}>Open</AppButton>
+
       <ChoosePlaylistModal
         open={openModal}
         setOpen={setOpenModal}
--- audio-hosting-client/src/pages/Main/styles.js
+++ audio-hosting-client/src/pages/Main/styles.js
@@ -8,9 +8,30 @@ const useStyles = makeStyles((theme) => ({
     gridTemplateColumns: "repeat(2, 1fr)",
     boxShadow: "2px 4px 27px 0px rgba(34, 60, 80, 0.2)",
     height: theme.constants.playlistHeight,
+    position: "relative",
   },
   playerContainer: {
     boxShadow: theme.constants.shadow,
+  },
+  controls: {
+    display: "flex",
+    marginBottom: theme.spacing(2),
+  },
+  marginRight: {
+    marginRight: theme.spacing(2)
+  },
+  drawer: {
+    maxWidth: "50%",
+  },
+  drawerPaper: {
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    border: "none",
+    maxWidth: "50%",
+    boxShadow: theme.constants.shadow
   },
 }));
 
