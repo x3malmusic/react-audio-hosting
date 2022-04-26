21f39245fb4357bc14108d846d72015564f917b7
x3malmusic <x3malmusic@gmail.com>
2022-04-26 19:11:16 +0300

    fix: modified saga error handler

--- audio-hosting-client/src/redux-store/sagas/error.js
+++ audio-hosting-client/src/redux-store/sagas/error.js
@@ -1,17 +1,18 @@
-import { SET_LOADING } from "../actions/types";
 import { put, fork } from "redux-saga/effects";
 import { notify } from "../../utils/notifications";
+import routines from "../actions/routines";
 
 const safeWrapper = function* (saga, ...rest) {
   const [args] = rest
+
   try {
-    yield put({ type: `${SET_LOADING}_${args.type}`, payload: true });
+    yield put({ type: routines[args.type].LOADING, payload: true });
     yield saga(args);
   } catch (err) {
     console.log('sagaWrapper', err)
     notify({ message: err.data?.message, type: 'danger', title: 'Error' });
   } finally {
-    yield put({ type: `${SET_LOADING}_${args.type}`, payload: false });
+    yield put({ type: routines[args.type].LOADING, payload: false });
   }
 };
 
