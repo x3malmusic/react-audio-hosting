00ba2976b61ee8534c388a2ba4468f8bc99e2cdb
x3malmusic <x3malmusic@gmail.com>
2022-04-26 19:11:40 +0300

    fix: modified loading reducer

--- audio-hosting-client/src/redux-store/reducers/loading.js
+++ audio-hosting-client/src/redux-store/reducers/loading.js
@@ -1,9 +1,7 @@
-import { SET_LOADING } from "../actions/types";
-
 const initialState = {};
 
 const loading = (state = initialState, action) => {
-  if (action.type.search(SET_LOADING) === 0) {
+  if (action.type.search("LOADING") > 0) {
     return {
       ...state,
       [action.type]: action.payload,
