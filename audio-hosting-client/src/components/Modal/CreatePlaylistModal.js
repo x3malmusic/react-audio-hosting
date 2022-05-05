d8f4fb7b786aeea4ea9e3403826e4e0069fed022
x3malmusic <x3malmusic@gmail.com>
2022-05-05 12:01:21 +0300

    feat: added form validation to create playlist modal

--- audio-hosting-client/src/components/Modal/CreatePlaylistModal.js
+++ audio-hosting-client/src/components/Modal/CreatePlaylistModal.js
@@ -1,50 +1,70 @@
 import React from "react";
-import { Dialog,
+import { 
+  Dialog,
   DialogActions,
   DialogContent,
   DialogContentText,
   DialogTitle,
-  TextField
+  TextField,
+  Typography,
 } from "@material-ui/core";
+import { Formik } from "formik";
 import AppButton from "../AppButton";
+import { createPlaylistSchema } from "../../utils/validationSchemas";
+import useStyles from "./styles"
 
-export default function Modal({ open, onClose, action, inputChange, title, inputValue, content, ...props }) {
+export default function Modal({ open, onClose, action, title, content, ...props }) {
+  const classes = useStyles()
 
-  const create = () => {
-    if (!inputValue || inputValue.length < 3) return
-
-    action()
+  const create = ({ name }) => {
+    action(name)
     onClose()
   }
 
-  const changeHandler = (e) => {
-    inputChange(e.target.value)
+  const keyListener = (e, submit) => {
+    if (e.key === "Enter") submit()
   }
 
   return (
-    <Dialog onClose={onClose} open={open} {...props}>
-      <DialogTitle>{title}</DialogTitle>
-      <DialogContent>
-        <DialogContentText>
-          {content}
-        </DialogContentText>
-        <TextField
-          autoFocus
-          margin="dense"
-          label="Playlist Name"
-          fullWidth
-          value={inputValue}
-          onChange={changeHandler}
-        />
-      </DialogContent>
-      <DialogActions>
-        <AppButton onClick={create}>
-          Create
-        </AppButton>
-        <AppButton onClick={onClose}>
-          Cancel
-        </AppButton>
-      </DialogActions>
-    </Dialog>
+    <Formik
+      initialValues={{ name: '' }}
+      validationSchema={createPlaylistSchema}
+      onSubmit={create}
+      validateOnChange={false}
+      validateOnBlur={false}
+    >
+      {({ errors, values, handleChange, handleSubmit, touched }) => (
+        <Dialog onClose={onClose} open={open} {...props}>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              {content}
+            </DialogContentText>
+
+            <TextField
+              id="name"
+              autoFocus
+              margin="dense"
+              label="Playlist Name"
+              fullWidth
+              error={!!errors.name}
+              value={values.name}
+              onChange={handleChange}
+              onKeyUp={e => keyListener(e, handleSubmit)}
+            />
+            <Typography className={classes.errorMessage} color="error">{touched.name && errors.name}</Typography>
+
+          </DialogContent>
+          <DialogActions>
+            <AppButton onClick={handleSubmit}>
+              Create
+            </AppButton>
+            <AppButton onClick={onClose}>
+              Cancel
+            </AppButton>
+          </DialogActions>
+        </Dialog>
+      )}
+    </Formik>
   )
 }
---
+++ audio-hosting-client/src/components/Modal/styles.js
@@ -0,0 +1,13 @@
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  errorMessage: {
+    fontSize: 12,
+    // marginBottom: theme.spacing(0.5),
+  },
+  input: {
+    marginTop: 0,
+  }
+}));
+
+export default useStyles;
